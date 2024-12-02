c94503c23f38443aa8acd3865afbb271b7208543
NicolasG <nicolas.gunst@decathlon.com>
2024-12-02 12:12:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AoC-2024
+
+This year I continue improving my Python skills thanks to the advent of code  
+Day 1 : Thanks to gSheets  
+Day 2 : 
---
+++ base.py
@@ -0,0 +1,12 @@
+puzzle_input = open("data.txt", "r").read().rstrip().splitlines()
+
+
+res1 = 0
+res2 = 0
+
+
+
+
+
+print("part 1 : ",res1)
+print("part 2 : ",res2)
